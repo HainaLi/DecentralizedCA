9a31a2a31c6f0297768f0ddcccf65ec3057e22fe
bargav <bj4nq@virginia.edu>
2017-04-10 15:56:03 -0400

    changed some code

--- src/oblivCA.c
+++ src/oblivCA.c
@@ -10,31 +10,38 @@ int main(int argc,char *argv[]){
   protocolIO io;
   clock_t start, end;
   double cpu_time_used;
+  int party;
   
   io.private_key_share1 = (char *) malloc(MAXN);
   io.private_key_share2 = (char *) malloc(MAXN);
-
-  if(argv[1][0] == '1')    
-    memcpy(io.private_key_share1, "abcd", MAXN);
-
-  else if (argv[1][0] == '2')    
-    memcpy(io.private_key_share2, "2222", MAXN);
-
-  start = clock();
+ 
   //protocolUseStdio(&pd);
+
+  if(strcmp(argv[3],"--")==0)
+    party = 1;
+  else
+    party = 2;
 
   const char* remote_host = (strcmp(argv[3],"--")==0?NULL:argv[3]);
   ocTestUtilTcpOrDie(&pd,remote_host,argv[1]);
 
+  if(party == 1)    
+    memcpy(io.private_key_share1, "abcd", MAXN);
 
-  setCurrentParty(&pd,argv[1][0]=='1'?1:2);
+  else if (party == 2)    
+    memcpy(io.private_key_share2, "2222", MAXN);
+
+  start = clock();
+
+  //setCurrentParty(&pd,argv[1][0]=='1'?1:2);
+  setCurrentParty(&pd,(remote_host?2:1));
   execYaoProtocol(&pd,signCertificate,&io);
   cleanupProtocol(&pd);
   end = clock();
 
   int gates = yaoGateCount();
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-  fprintf(stderr,"\nParty %c, Elapsed Time: %f seconds\n", argv[1][0], cpu_time_used);
+  fprintf(stderr,"\nParty %d, Elapsed Time: %f seconds\n", party, cpu_time_used);
   fprintf(stderr,"\nNumber of Gates: %u\n", gates);
 
   io.private_key[MAXN-1] = '\0'; // temp code for check, shoudl be removed
