6cf3d56089227e04709d9e41b4f20d1997e1d225
Hannah <haina.li24@gmail.com>
2017-04-22 13:02:28 -0400

    offline SHA256 hashing (Step 4)

--- src/hashMessage.c
+++ src/hashMessage.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "sha256.c"
+#include <memory.h>
+#include "sha256.h"
 
 void print_hash(unsigned char hash[])
 {
@@ -11,34 +12,6 @@ void print_hash(unsigned char hash[])
    printf("\n");
 }
 
-char * hashMessage(char * message) {
-
-	unsigned char hash[32];
-	SHA256_CTX ctx;
-
-	// Hash one
-	sha256_init(&ctx);
-	sha256_update(&ctx,message,strlen(message));
-	sha256_final(&ctx,hash);
-	print_hash(hash);
-
-	return hash;
-	
-}
-/*
-char * bit_string_to_octet_string_conversion() {
-    long int binarynum, octalnum = 0, j = 1, remainder;
- 
-    while (binarynum != 0)
-    {
-        remainder = binarynum % 10;
-        octalnum = octalnum + remainder * j;
-        j = j * 2;
-        binarynum = binarynum / 10;
-    }
-    return octalnum; 
-}
-*/
 char* stringToBinary(char* s) {
     if(s == NULL) return 0; /* no input string */
     size_t len = strlen(s);
@@ -57,10 +30,54 @@ char* stringToBinary(char* s) {
     return binary;
 }
 
+//outputs the hashed value
+char * hashMessage(char * message) {
+    int str_len = strlen(message);
+    long hashmaxlen = 2.305843e18;
+
+    //page 31. Step 1
+    //str_len shouldn't be longer than hashmaxlen = (2^61)-1 = 2.305843e18
+    if (str_len >= hashmaxlen){
+        printf("%s\n", "Invalid. Message length is too long. Exiting.");
+        exit(-1);
+    }
+    //unsigned char text1[]={"abc"};
+    //page 31. Step 2
+    //Convert M to bit string M'
+    char * bit_string_message = stringToBinary(message);
+
+    //Step 3. 
+    //H = Hash(M')
+	//unsigned char hash[SHA256_BLOCK_SIZE];
+    unsigned char * hash = malloc(SHA256_BLOCK_SIZE);
+	SHA256_CTX ctx;
+	sha256_init(&ctx);
+	sha256_update(&ctx,bit_string_message,strlen(bit_string_message));
+	sha256_final(&ctx,hash);
+    //print_hash(hash);
+
+	return hash;
+	
+}
+/*
+char * bit_string_to_octet_string_conversion() {
+    long int binarynum, octalnum = 0, j = 1, remainder;
+ 
+    while (binarynum != 0)
+    {
+        remainder = binarynum % 10;
+        octalnum = octalnum + remainder * j;
+        j = j * 2;
+        binarynum = binarynum / 10;
+    }
+    return octalnum; 
+}
+*/
+
+
 int main(int argc,char *argv[]) {
-	char * binary = stringToBinary("hello");
-	printf("%s\n", binary);
-	hashMessage("binary"); 
+    //SHA256 can only hash strings of length less than hashmaxlen = (2^61)-1 = 2.305843e18
+	char * result = hashMessage("hello"); 
 
 	return 0;
 }
--- src/sha256.c
+++ src/sha256.c
@@ -1,10 +1,23 @@
-#include <stdio.h> 
+/*********************************************************************
+* Filename:   sha256.c
+* Author:     Brad Conte (brad AT bradconte.com)
+* Copyright:
+* Disclaimer: This code is presented "as is" without any guarantees.
+* Details:    Implementation of the SHA-256 hashing algorithm.
+              SHA-256 is one of the three algorithms in the SHA2
+              specification. The others, SHA-384 and SHA-512, are not
+              offered in this implementation.
+              Algorithm specification can be found here:
+               * http://csrc.nist.gov/publications/fips/fips180-2/fips180-2withchangenotice.pdf
+              This implementation uses little endian byte order.
+*********************************************************************/
 
-#define uchar unsigned char // 8-bit byte
-#define uint unsigned int // 32-bit word
+/*************************** HEADER FILES ***************************/
+#include <stdlib.h>
+#include <memory.h>
+#include "sha256.h"
 
-// DBL_INT_ADD treats two unsigned ints a and b as one 64-bit integer and adds c to it
-#define DBL_INT_ADD(a,b,c) if (a > 0xffffffff - (c)) ++b; a += c;
+/****************************** MACROS ******************************/
 #define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
 #define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
 
@@ -15,14 +28,8 @@
 #define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
 #define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
 
-typedef struct {
-   uchar data[64];
-   uint datalen;
-   uint bitlen[2];
-   uint state[8];
-} SHA256_CTX;
-
-uint k[64] = {
+/**************************** VARIABLES *****************************/
+static const WORD k[64] = {
    0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
    0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
    0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
@@ -33,15 +40,15 @@ uint k[64] = {
    0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
 };
 
+/*********************** FUNCTION DEFINITIONS ***********************/
+void sha256_transform(SHA256_CTX *ctx, const BYTE data[])
+{
+   WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
 
-void sha256_transform(SHA256_CTX *ctx, uchar data[])
-{  
-   uint a,b,c,d,e,f,g,h,i,j,t1,t2,m[64];
-      
-   for (i=0,j=0; i < 16; ++i, j += 4)
-      m[i] = (data[j] << 24) | (data[j+1] << 16) | (data[j+2] << 8) | (data[j+3]);
+   for (i = 0, j = 0; i < 16; ++i, j += 4)
+      m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
    for ( ; i < 64; ++i)
-      m[i] = SIG1(m[i-2]) + m[i-7] + SIG0(m[i-15]) + m[i-16];
+      m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
 
    a = ctx->state[0];
    b = ctx->state[1];
@@ -51,7 +58,7 @@ void sha256_transform(SHA256_CTX *ctx, uchar data[])
    f = ctx->state[5];
    g = ctx->state[6];
    h = ctx->state[7];
-   
+
    for (i = 0; i < 64; ++i) {
       t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
       t2 = EP0(a) + MAJ(a,b,c);
@@ -64,7 +71,7 @@ void sha256_transform(SHA256_CTX *ctx, uchar data[])
       b = a;
       a = t1 + t2;
    }
-   
+
    ctx->state[0] += a;
    ctx->state[1] += b;
    ctx->state[2] += c;
@@ -73,13 +80,12 @@ void sha256_transform(SHA256_CTX *ctx, uchar data[])
    ctx->state[5] += f;
    ctx->state[6] += g;
    ctx->state[7] += h;
-}  
+}
 
 void sha256_init(SHA256_CTX *ctx)
-{  
-   ctx->datalen = 0; 
-   ctx->bitlen[0] = 0; 
-   ctx->bitlen[1] = 0; 
+{
+   ctx->datalen = 0;
+   ctx->bitlen = 0;
    ctx->state[0] = 0x6a09e667;
    ctx->state[1] = 0xbb67ae85;
    ctx->state[2] = 0x3c6ef372;
@@ -90,63 +96,63 @@ void sha256_init(SHA256_CTX *ctx)
    ctx->state[7] = 0x5be0cd19;
 }
 
-void sha256_update(SHA256_CTX *ctx, uchar data[], uint len)
-{  
-   uint t,i;
-   
-   for (i=0; i < len; ++i) { 
-      ctx->data[ctx->datalen] = data[i]; 
-      ctx->datalen++; 
-      if (ctx->datalen == 64) { 
-         sha256_transform(ctx,ctx->data);
-         DBL_INT_ADD(ctx->bitlen[0],ctx->bitlen[1],512); 
-         ctx->datalen = 0; 
-      }  
-   }  
-}  
+void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len)
+{
+   WORD i;
 
-void sha256_final(SHA256_CTX *ctx, uchar hash[])
-{  
-   uint i; 
-   
-   i = ctx->datalen; 
-   
-   // Pad whatever data is left in the buffer. 
-   if (ctx->datalen < 56) { 
-      ctx->data[i++] = 0x80; 
-      while (i < 56) 
-         ctx->data[i++] = 0x00; 
-   }  
-   else { 
-      ctx->data[i++] = 0x80; 
-      while (i < 64) 
-         ctx->data[i++] = 0x00; 
-      sha256_transform(ctx,ctx->data);
-      memset(ctx->data,0,56); 
-   }  
-   
-   // Append to the padding the total message's length in bits and transform. 
-   DBL_INT_ADD(ctx->bitlen[0],ctx->bitlen[1],ctx->datalen * 8);
-   ctx->data[63] = ctx->bitlen[0]; 
-   ctx->data[62] = ctx->bitlen[0] >> 8; 
-   ctx->data[61] = ctx->bitlen[0] >> 16; 
-   ctx->data[60] = ctx->bitlen[0] >> 24; 
-   ctx->data[59] = ctx->bitlen[1]; 
-   ctx->data[58] = ctx->bitlen[1] >> 8; 
-   ctx->data[57] = ctx->bitlen[1] >> 16;  
-   ctx->data[56] = ctx->bitlen[1] >> 24; 
-   sha256_transform(ctx,ctx->data);
-   
+   for (i = 0; i < len; ++i) {
+      ctx->data[ctx->datalen] = data[i];
+      ctx->datalen++;
+      if (ctx->datalen == 64) {
+         sha256_transform(ctx, ctx->data);
+         ctx->bitlen += 512;
+         ctx->datalen = 0;
+      }
+   }
+}
+
+void sha256_final(SHA256_CTX *ctx, BYTE hash[])
+{
+   WORD i;
+
+   i = ctx->datalen;
+
+   // Pad whatever data is left in the buffer.
+   if (ctx->datalen < 56) {
+      ctx->data[i++] = 0x80;
+      while (i < 56)
+         ctx->data[i++] = 0x00;
+   }
+   else {
+      ctx->data[i++] = 0x80;
+      while (i < 64)
+         ctx->data[i++] = 0x00;
+      sha256_transform(ctx, ctx->data);
+      memset(ctx->data, 0, 56);
+   }
+
+   // Append to the padding the total message's length in bits and transform.
+   ctx->bitlen += ctx->datalen * 8;
+   ctx->data[63] = ctx->bitlen;
+   ctx->data[62] = ctx->bitlen >> 8;
+   ctx->data[61] = ctx->bitlen >> 16;
+   ctx->data[60] = ctx->bitlen >> 24;
+   ctx->data[59] = ctx->bitlen >> 32;
+   ctx->data[58] = ctx->bitlen >> 40;
+   ctx->data[57] = ctx->bitlen >> 48;
+   ctx->data[56] = ctx->bitlen >> 56;
+   sha256_transform(ctx, ctx->data);
+
    // Since this implementation uses little endian byte ordering and SHA uses big endian,
-   // reverse all the bytes when copying the final state to the output hash. 
-   for (i=0; i < 4; ++i) { 
-      hash[i]    = (ctx->state[0] >> (24-i*8)) & 0x000000ff; 
-      hash[i+4]  = (ctx->state[1] >> (24-i*8)) & 0x000000ff; 
-      hash[i+8]  = (ctx->state[2] >> (24-i*8)) & 0x000000ff;
-      hash[i+12] = (ctx->state[3] >> (24-i*8)) & 0x000000ff;
-      hash[i+16] = (ctx->state[4] >> (24-i*8)) & 0x000000ff;
-      hash[i+20] = (ctx->state[5] >> (24-i*8)) & 0x000000ff;
-      hash[i+24] = (ctx->state[6] >> (24-i*8)) & 0x000000ff;
-      hash[i+28] = (ctx->state[7] >> (24-i*8)) & 0x000000ff;
-   }  
-}  
+   // reverse all the bytes when copying the final state to the output hash.
+   for (i = 0; i < 4; ++i) {
+      hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
+      hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
+      hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
+      hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
+      hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
+      hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
+      hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
+      hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
+   }
+}
